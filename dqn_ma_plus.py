45264898b9fd93528ffd970281a789268e1f022b
WangWen <wenwenla@outlook.com>
2021-07-26 20:20:53 +0800

    add env #20200726

---
+++ dqn_ma_plus.py
@@ -0,0 +1,275 @@
+import os
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torch.utils.tensorboard import SummaryWriter
+
+from collections import deque
+
+from replay_buffer import ReplayBuffer
+from zgkx_env2 import AttackEnv
+
+from entropy_stat import get_rao_quadratic_entropy, get_expected_entropy_over_states, get_det_diversity
+
+import argparse
+
+
+parser = argparse.ArgumentParser(description='[agents] [epsilon] [boom_step] [boom_range] [boom_cnt]')
+parser.add_argument('--agents', type=int, default=20)
+parser.add_argument('--epsilon', type=float, default=1.)
+parser.add_argument('--boom_step', type=int, default=20)
+parser.add_argument('--boom_range', type=int, default=2)
+parser.add_argument('--boom_cnt', type=int, default=2)
+parser.add_argument('--folder', type=str, default='logs')
+parser.add_argument('--history', type=int, default=10)
+parser.add_argument('--max_ep', type=int, default=500)
+parser.add_argument('--group', type=int, default=1)
+
+
+args = parser.parse_args()
+
+EPSILON = 0.1
+LR = 1e-3
+BATCH = 128
+GAMMA = 0.95
+START_SAMPLES = 2000
+REPLAY_BUFFER_SIZE = int(1e6)
+RENDER_FLAG = False
+FOLDER = args.folder
+HISTORY_LEN = args.history
+AGENTS = args.agents
+MAX_EP = args.max_ep
+GROUP_SZ = args.group
+
+
+class DQNModel(torch.nn.Module):
+
+    def __init__(self, obs_dim, act_cnt):
+        super().__init__()
+        self.fc1 = nn.Linear(obs_dim, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.q = nn.Linear(64, act_cnt)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        q_val = self.q(x)
+        return q_val
+
+
+class Policies:
+
+    def __init__(self, obs_dim, act_cnt, policy_state_dict):
+        self._model = DQNModel(obs_dim, act_cnt)
+        self._model.load_state_dict(policy_state_dict)
+
+    def choose_actions(self, states):
+        states = torch.tensor(states).float()
+        with torch.no_grad():
+            actions = self._model(states).numpy()
+        return np.argmax(actions, axis=1)
+
+
+class DQNAgent:
+
+    def __init__(self, obs_dim, act_cnt):
+        self._obs_dim = obs_dim
+        self._act_cnt = act_cnt
+        self.qnet = DQNModel(self._obs_dim, self._act_cnt)
+        self.target_qnet = DQNModel(self._obs_dim, self._act_cnt)
+        self.target_qnet.load_state_dict(self.qnet.state_dict())
+        self.optim = optim.Adam(self.qnet.parameters(), lr=LR)
+        self.loss_fn = torch.nn.MSELoss()
+        self.replay = ReplayBuffer(REPLAY_BUFFER_SIZE, (self._obs_dim, ), 1)
+        self._steps = 0
+
+    def get_state_dict(self):
+        return self.qnet.state_dict()
+
+    def choose_actions(self, states):
+        states = torch.tensor(states).float()
+        with torch.no_grad():
+            actions = self.qnet(states).numpy()
+        return np.argmax(actions, axis=1)
+
+    def choose_action(self, state):
+        state = torch.Tensor([state]).float()
+        with torch.no_grad():
+            action = self.qnet(state).numpy()
+        return np.argmax(action)
+
+    def choose_action_with_epsilon(self, state):
+        if np.random.uniform() < EPSILON:
+            return np.random.randint(0, self._act_cnt)
+        return self.choose_action(state)
+
+    def add(self, s, a, r, s_):
+        self.replay.add(s, a, r, s_)
+
+    def update(self):
+        self._steps += 1
+        if self.replay.n_samples() < START_SAMPLES:
+            return
+        s, a, r, s_ = self.replay.sample(BATCH)
+        with torch.no_grad():
+            target = self.qnet(torch.Tensor(s))
+            nxt_a = self.qnet(torch.Tensor(s_)).argmax(axis=1)
+            nxt_q = self.target_qnet(torch.Tensor(s_))
+            z = []
+            for i, v in enumerate(nxt_a.numpy()):
+                z.append(nxt_q[i, v])
+            nxt_q = torch.Tensor(z)
+            upd = GAMMA * nxt_q
+            upd = torch.Tensor(r) + upd
+            for i, v in enumerate(a):
+                target[i, v] = upd[i]
+        self.optim.zero_grad()
+        q = self.qnet(torch.Tensor(s))
+        loss = self.loss_fn(q, target)
+        loss.backward()
+        self.optim.step()
+        self.soft_copy_parm()
+        # if self._steps % 1000 == 0: NO LOG is NEEDED
+        #     self.sw.add_scalar('loss/qloss', loss.item(), self._steps)
+
+    def soft_copy_parm(self):
+        with torch.no_grad():
+            for t, s in zip(self.target_qnet.parameters(), self.qnet.parameters()):
+                t.copy_(0.01 * t.data + 0.99 * s.data)
+
+    def parameters(self):
+        return self.qnet.state_dict()
+
+
+def make_env(kwargs):
+    return AttackEnv(**kwargs)
+
+
+class DQNTrainer:
+
+    def __init__(self, env_config):
+        self._n_agents = env_config['n_agents']
+        self._env = make_env(env_config)
+
+        real_policy = [DQNAgent(self._env.obs_dim, self._env.act_cnt)
+                       for _ in range((self._n_agents + GROUP_SZ - 1) // GROUP_SZ)]
+        self._policy_mapper = {}
+        for i in range(self._n_agents):
+            self._policy_mapper[f'uav_{i}'] = real_policy[i // GROUP_SZ]
+
+        self._sampled_states = deque([], maxlen=100000)
+        self._policies = [deque([], maxlen=HISTORY_LEN) for _ in range(env_config['n_agents'])]
+        self._sw = SummaryWriter(os.path.join(FOLDER, 'logs'))
+        self._ep = 0
+
+    def train_one_ep(self):
+        self._ep += 1
+        states = self._env.reset()
+
+        for sampled_state in states.values():
+            if AttackEnv.is_alive(sampled_state):
+                self._sampled_states.append(sampled_state)
+
+        done = False
+        rew = 0
+        while not done:
+            agents = self._env.agents_name()
+            actions = {}
+            for agent in agents:
+                p = self._policy_mapper[agent]
+                actions[agent] = p.choose_action_with_epsilon(states[agent])
+
+            next_states, rewards, done, info = self._env.step(actions)
+
+            for sampled_state in next_states.values():
+                if AttackEnv.is_alive(sampled_state):
+                    self._sampled_states.append(sampled_state)
+
+            if RENDER_FLAG:
+                self._env.render()
+
+            for agent in agents:
+                p = self._policy_mapper[agent]
+                p.add(states[agent], actions[agent], rewards[agent], next_states[agent])
+                p.update()
+
+            states = next_states
+            rew += np.mean(list(rewards.values()))
+
+        for i, agent in enumerate(self._env.agents_name()):
+            p = self._policy_mapper[agent]
+            self._policies[i].append(Policies(self._env.obs_dim, self._env.act_cnt, p.get_state_dict()))
+
+        indices = np.random.randint(0, len(self._sampled_states), (200, ))
+        sampled_states = []
+        for p in indices:
+            sampled_states.append(self._sampled_states[p])
+
+        rao = self.log_rao_entropy(sampled_states)
+        avg_e = self.log_average_entropy(sampled_states)
+
+        self._sw.add_scalar('rewards', rew, self._ep)
+        self._sw.add_scalar('rao', rao, self._ep)
+        self._sw.add_scalar('avg', avg_e, self._ep)
+
+        if self._ep > 15:
+            det_e = self.log_det_diversity(sampled_states)
+            self._sw.add_scalar('det', det_e, self._ep)
+        else:
+            self._sw.add_scalar('det', 0.5, self._ep)  # TODO: HACK
+
+        # self.time_step_log()
+
+        return rew, rao, avg_e
+
+    def log_rao_entropy(self, sampled_states):
+        policies = []
+        for i in self._env.agents_name():
+            policies.append(self._policy_mapper[i])
+        return get_rao_quadratic_entropy(policies, sampled_states)
+
+    def log_average_entropy(self, sampled_states):
+        policies = []
+        for i in self._env.agents_name():
+            policies.append(self._policy_mapper[i])
+        return get_expected_entropy_over_states(policies, sampled_states)
+
+    def log_det_diversity(self, sampled_states):
+        policies = []
+        for i in self._env.agents_name():
+            policies.append(self._policy_mapper[i])
+        return get_det_diversity(policies, sampled_states)
+
+    def time_step_log(self):
+        for i in range(self._n_agents):
+            rao = get_rao_quadratic_entropy(self._policies[i], self._sampled_states)
+            avg_e = get_expected_entropy_over_states(self._policies[i], self._sampled_states)
+            self._sw.add_scalar(f'agent_{i}/rao', rao, self._ep)
+            self._sw.add_scalar(f'agent_{i}/avg_e', avg_e, self._ep)
+            # self._sw.add_scalar(f'agent_{i}/det', avg_e, self._ep)
+
+    def save_model(self):
+        torch.save([v.parameters() for v in self._policy_mapper.values()], f'{FOLDER}/{self._ep}.pkl')
+
+
+def main():
+    torch.set_num_threads(1)
+    trainer = DQNTrainer({
+        'n_agents': AGENTS,
+        'attack_epsilon': args.epsilon,
+        'boom_step': args.boom_step,
+        'boom_range': args.boom_range,
+        'boom_cnt': args.boom_cnt
+    })
+    for i in range(MAX_EP):
+        r = trainer.train_one_ep()
+        print(f'EP: {i} RE: {r}')
+        if i % 100 == 0:
+            trainer.save_model()
+
+
+if __name__ == '__main__':
+    main()
---
+++ zgkx_env2.py
@@ -0,0 +1,223 @@
+import copy
+import itertools
+import math
+import time
+
+from util import *
+from PIL import Image
+
+import numpy as np
+
+
+def get_dist(point0, point1):
+    return abs(point1[0] - point0[0]) + abs(point1[1] - point0[1])
+
+
+class AttackEnv:
+
+    MAX_SIZE_ROW = 20
+    MAX_SIZE_COL = 20
+    ATTACK_PENALTY = 200
+    VIEW_RANGE = 3
+
+    @staticmethod
+    def is_alive(state):
+        return abs(state[-3] - 1.0) < 1e-6
+
+    def __init__(self, n_agents, attack_epsilon, boom_step, boom_range, boom_cnt):
+        self._n_agents = n_agents
+        self._pos = [
+            [0, 0] for _ in range(n_agents)
+        ]
+        self._alive = [
+            True for _ in range(n_agents)
+        ]
+        self._attack_epsilon = attack_epsilon
+        self._boom_step = boom_step
+        self._boom_range = boom_range
+        self._boom_cnt = boom_cnt
+        self._step = 0
+        self._boom_areas = []
+        self._bit_cnt = None
+        self._viewer = None
+
+        self.obs_dim = (2 * AttackEnv.VIEW_RANGE + 1) ** 2 + 3
+        self.act_cnt = 5
+        self.n_agents = n_agents
+
+    def agents_name(self):
+        return [f'uav_{i}' for i in range(self._n_agents)]
+
+    def get_observation(self):
+        # 0: empty, 1: friends, 2: boom
+        target_position = [AttackEnv.MAX_SIZE_ROW - 1, AttackEnv.MAX_SIZE_COL - 1]
+        bitmap = np.zeros((AttackEnv.MAX_SIZE_ROW, AttackEnv.MAX_SIZE_COL))
+        for i in range(self._n_agents):
+            bitmap[self._pos[i][0], self._pos[i][1]] = 1
+        for bp in self._boom_areas:
+            bitmap[bp[0]: bp[0] + self._boom_range, bp[1]: bp[1] + self._boom_range] = 2
+        bitmap = np.pad(bitmap, AttackEnv.VIEW_RANGE, 'constant', constant_values=-1)
+        obs = {}
+        for i in range(self._n_agents):
+            code_len = (2 * AttackEnv.VIEW_RANGE + 1) ** 2
+            now_view = bitmap[self._pos[i][0]: self._pos[i][0] + 2 * AttackEnv.VIEW_RANGE + 1,
+                       self._pos[i][1]: self._pos[i][1] + 2 * AttackEnv.VIEW_RANGE + 1].reshape((code_len, ))
+            obs[f'uav_{i}'] = np.hstack([now_view, [int(self._alive[i] or self._pos[i] == target_position), self._pos[i][0], self._pos[i][1]]])
+        return obs
+
+    def get_reward(self, prev_positions, now_positions):
+        rewards = [0 for _ in range(self._n_agents)]
+        target_position = [AttackEnv.MAX_SIZE_ROW - 1, AttackEnv.MAX_SIZE_COL - 1]
+        # distance based reward
+        for i in range(self._n_agents):
+            dist_prev = get_dist(prev_positions[i], target_position)
+            dist_next = get_dist(now_positions[i], target_position)
+            rewards[i] += dist_prev - dist_next
+        # attack based reward
+        atk_list = []
+        for i in range(self._n_agents):
+            attacked = False
+            for j in range(len(self._boom_areas)):
+                if self._boom_areas[j][0] <= self._pos[i][0] < self._boom_areas[j][0] + self._boom_range and \
+                        self._boom_areas[j][1] <= self._pos[i][1] < self._boom_areas[j][1] + self._boom_range:
+                    attacked = True
+                    break
+            if attacked:
+                rewards[i] -= AttackEnv.ATTACK_PENALTY
+                atk_list.append(i)
+        # time based reward
+        for i in range(self._n_agents):
+            rewards[i] -= 1
+
+        for i in range(self._n_agents):
+            if now_positions[i] == target_position:
+                rewards[i] = 0
+            if not self._alive[i]:
+                rewards[i] = 0
+
+        for atk in atk_list:
+            self._alive[atk] = False
+
+        return {
+            f'uav_{i}': rewards[i] for i in range(self._n_agents)
+        }
+
+    def reset(self):
+        self._step = 0
+        self._pos = [
+            [0, 0] for _ in range(self._n_agents)
+        ]
+        self._alive = [
+            True for _ in range(self._n_agents)
+        ]
+        return self.get_observation()
+
+    def step(self, actions):
+        dirs = [[0, 0], [-1, 0], [1, 0], [0, -1], [0, 1]]
+
+        prev_pos = copy.deepcopy(self._pos)
+
+        for i, uav_name in enumerate(self.agents_name()):
+            if not self._alive[i]:
+                continue
+            if self._pos[i][0] == AttackEnv.MAX_SIZE_ROW - 1 and self._pos[i][1] == AttackEnv.MAX_SIZE_COL - 1:
+                continue
+
+            if 0 <= self._pos[i][0] + dirs[actions[uav_name]][0] < AttackEnv.MAX_SIZE_ROW:
+                self._pos[i][0] += dirs[actions[uav_name]][0]
+
+            if 0 <= self._pos[i][1] + dirs[actions[uav_name]][1] < AttackEnv.MAX_SIZE_COL:
+                self._pos[i][1] += dirs[actions[uav_name]][1]
+
+        self._bit_cnt = np.zeros((AttackEnv.MAX_SIZE_ROW, AttackEnv.MAX_SIZE_COL), int)
+
+        for i, p in enumerate(self._pos):
+            if self._alive[i] and 1 <= p[1] < AttackEnv.MAX_SIZE_COL - 1:
+                self._bit_cnt[p[0], p[1]] += 1
+
+        if self._step % self._boom_step == 0:
+            # generate boom areas
+            self._boom_areas.clear()
+
+            attack_choices = []
+            for row in range(0, AttackEnv.MAX_SIZE_ROW - self._boom_range):
+                for col in range(1, AttackEnv.MAX_SIZE_COL - self._boom_range - 1):
+                    cnt = 0
+                    for dr, dc in itertools.product(range(self._boom_range), range(self._boom_range)):
+                        cnt += self._bit_cnt[row + dr, col + dc]
+                    attack_choices.append((cnt, row, col))
+
+            attack_choices.sort(key=lambda x: (-x[0], x[1], x[2]))
+            attack_choices_index = 0
+            for i in range(self._boom_cnt):
+                if np.random.uniform() < self._attack_epsilon:
+                    self._boom_areas.append(
+                        [
+                            np.random.randint(0, AttackEnv.MAX_SIZE_ROW - self._boom_range),
+                            np.random.randint(1, AttackEnv.MAX_SIZE_COL - self._boom_range - 1),
+                        ]
+                    )
+                else:
+                    self._boom_areas.append(
+                        [
+                            attack_choices[attack_choices_index][1],
+                            attack_choices[attack_choices_index][2]
+                        ]
+                    )
+                    attack_choices_index += 1
+
+        self._step += 1
+
+        obs = self.get_observation()
+        rew = self.get_reward(prev_pos, self._pos)
+
+        all_die = True
+        for i in range(self._n_agents):
+            if self._alive[i] and self._pos[i] != [AttackEnv.MAX_SIZE_ROW - 1, AttackEnv.MAX_SIZE_COL - 1]:
+                all_die = False
+                break
+
+        return obs, rew, self._step == 200 or all_die, {}
+
+    def render(self, mode='human'):
+        cell_size = 1000 // max(AttackEnv.MAX_SIZE_ROW, AttackEnv.MAX_SIZE_COL)
+        image = Image.new(mode='RGB', size=(1000, 1000), color='white')
+        draw_grid(image, AttackEnv.MAX_SIZE_ROW, AttackEnv.MAX_SIZE_COL, 1000 // max(AttackEnv.MAX_SIZE_ROW, AttackEnv.MAX_SIZE_COL))
+
+        for pos in self._boom_areas:
+            for dr, dc in itertools.product(range(self._boom_range), range(self._boom_range)):
+                draw_ball(image, pos[0] + dr, pos[1] + dc, cell_size, 'red')
+
+        for i, pos in enumerate(self._pos):
+            draw_ball(image, pos[0], pos[1], cell_size, 'blue' if self._alive[i] else 'black')
+
+        image = np.asarray(image)
+        if mode == 'rgb_array':
+            return image
+        elif mode == 'human':
+            from gym.envs.classic_control import rendering
+            if self._viewer is None:
+                self._viewer = rendering.SimpleImageViewer()
+            self._viewer.imshow(image)
+            return self._viewer.isopen
+
+
+def main():
+    env = AttackEnv(10, 1.0, 10, 2, 2)
+    env.reset()
+    done = False
+    pre = time.time()
+
+    while not done:
+        _, _, done, _ = env.step({
+            f'uav_{i}': np.random.randint(0, 5) for i in range(env._n_agents)
+        })
+
+        # env.render()
+        # time.sleep(0.2)
+    nxt = time.time()
+    print(f'Time: {nxt - pre}')
+
+
+if __name__ == '__main__':
+    main()
